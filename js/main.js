e3c73e92bcbf60a072399ae486137b173bcaea73
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2021-07-12 18:15:44 -0500

    ultimate fixes
    👽

--- js/main.js
+++ js/main.js
@@ -1,87 +1,88 @@
-$(document).ready( () => {
-    var height = $('.menu').offset().top;
+$(document).ready(() => {
+  const height = $(".menu").offset().top;
 
-    $(window).on('scroll', () => {
-        if ($(window).scrollTop() > height) {
-            $('.menu').addClass('menufixed');
-        } else {
-            $('.menu').removeClass('menufixed');
-            $('.navmenu').removeClass('active');
-            $('.hamburguer').removeClass('active');
-        }
-    });
+  $(window).on("scroll", () => {
+    if ($(window).scrollTop() > height) {
+      $(".menu").addClass("menufixed");
+    } else {
+      $(".menu").removeClass("menufixed");
+      $(".navmenu").removeClass("active");
+      $(".hamburguer").removeClass("active");
+    }
+  });
 });
 
-const boxes = document.querySelectorAll('.boxes');
-const box = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll(".boxes");
+const box = document.querySelectorAll(".box");
 
-box.forEach((bloque, i) => {
-    box[i].addEventListener('click', () => {
-        boxes.forEach((conts, i) => {
-            boxes[i].classList.remove('enable');
-        });
-        boxes[i].classList.add('enable');
+box.forEach((bloque, item) => {
+  box[item].addEventListener("click", () => {
+    boxes.forEach((conts, item) => {
+      boxes[item].classList.remove("enable");
     });
+    boxes[item].classList.add("enable");
+  });
 });
 
-window.addEventListener('load', () => {
-    new Glider(document.querySelector('.testimonial'), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        scrollLock: true,
-        dots: '.points',
-        arrows: {
-            prev: '.previus',
-            next: '.next',
+window.addEventListener("load", () => {
+  new Glider(document.querySelector(".testimonial"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    dots: ".points",
+    arrows: {
+      prev: ".previus",
+      next: ".next",
+    },
+    responsive: [
+      {
+        // screens greater than >576px
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          itemWidth: 250,
+          duration: 0.25,
         },
-        responsive: [{
-            // screens greater than >576px
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                itemWidth: 250,
-                duration: 0.25,
-            },
-        }, ],
-    });
+      },
+    ],
+  });
 });
 
-const navItem = document.querySelectorAll('.navitem');
+const navItem = document.querySelectorAll(".navitem");
 
-navItem.forEach((item, i) => {
-    navItem[i].addEventListener('click', () => {
-
-        navItem.forEach((item, i) => {
-            navItem[i].classList.remove('actived');
-        });
-        navItem[i].classList.add('actived');
+navItem.forEach((item, iterator) => {
+  navItem[iterator].addEventListener("click", () => {
+    navItem.forEach((item, iterator) => {
+      navItem[iterator].classList.remove("actived");
     });
+    navItem[iterator].classList.add("actived");
+  });
 });
 
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.navmenu');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navmenu");
 
-hamburger.addEventListener('click', mobileTabMenu);
+hamburger.addEventListener("click", mobileTabMenu);
 
 function mobileTabMenu() {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll('.navlink');
+const navLink = document.querySelectorAll(".navlink");
 
-navLink.forEach((nav) => nav.addEventListener('click', closeMenu));
+navLink.forEach((nav) => nav.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
-const contact = document.querySelector('.button2');
+const contact = document.querySelector(".button2");
 
-contact.addEventListener('click', visited);
+contact.addEventListener("click", visited);
 
 function visited() {
-    contact.classList.add('visited');
+  contact.classList.add("visited");
 }
